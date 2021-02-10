5aab5b42db725b64ed395637ca6c2fa76ce8f5b5
Leonardo F. Nascimento <leofn3@gmail.com>
2021-02-10 15:37:16 -0300

    Update CSVTOTXT_purrr.R

--- R/CSVTOTXT_purrr.R
+++ R/CSVTOTXT_purrr.R
@@ -1,17 +1,15 @@
+######### Função para transformar um vetor de strings em txt individuais para cada linha by https://github.com/jjesusfilho
 
-
-purrr::walk(1:nrow(vacina),~{
-  
-  nome <- vacina %>% 
-    dplyr::slice(.x) %>% 
-    dplyr::pull("nome")
-  
-  
-  vacina %>% 
+purrr::walk(1:nrow(df),~{
+  ## Escolher um vector do df com os nomes do arquivos txts que serão criados 
+  nome <- df %>% 
+  dplyr::slice(.x) %>% 
+  dplyr::pull("nome")
+  ## Selecionar o df 
+  df %>% 
     dplyr::slice(.x) %>% 
     dplyr::pull("texto") %>% 
     write(file = paste0(nome,".txt"))
-  
-})
+  })
 
-Louvo o Pai, louvo o filho, louvo à última Trindade!
+### Os txts serão criados no 
